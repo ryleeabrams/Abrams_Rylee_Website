e07400e34c0954508a377f76248fef68630932dd
ryleeabrams <157652786+ryleeabrams@users.noreply.github.com>
2024-04-19 15:37:20 -0700

    website deploy 1

---
+++ js/index.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.child-container', {
+    y: 100,
+    duration: 2.5,
+    ease: "power4.out",
+    stagger: 0.5,
+    scrollTrigger: {
+        trigger:'.child-container',
+        markers: true, 
+    } 
+});
+
+gsap.from('.hero-image', {
+    xPercent: -100,
+    duration: 2.5,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger:'.hero-image',
+        markers: true, 
+    } 
+});
